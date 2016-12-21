3cf049e200451723a4aea9fce0ebe017c6ab4120
Edwin <e.malisevskis@gmail.com>
2016-12-22 01:16:54 +0200

    inital commit

---
+++ src/test/java/lv/rtu/AdasApplicationTests.java
@@ -0,0 +1,16 @@
+package lv.rtu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
